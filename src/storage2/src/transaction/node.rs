4206e24a4c44facb8f0bdcde13858b98152c8518
power1628 <pgplus1628@qq.com>
2025-10-14 16:54:05 +0800

    feat(storage2): add node and relationship mutation method stubs

--- src/storage2/src/transaction/node.rs
+++ src/storage2/src/transaction/node.rs
@@ -1,6 +1,6 @@
-use mojito_common::{LabelId, NodeId, PropertyKeyId, value::Value};
+use mojito_common::{LabelId, NodeId, PropertyKeyId, store_types::PropertyValue, value::Value};
 
-use crate::{error::GraphStoreError, transaction::Transaction};
+use crate::{error::GraphStoreError, graph::GraphStore, transaction::Transaction};
 
 impl Transaction {
     pub fn node_create(
@@ -8,6 +8,41 @@ impl Transaction {
         labels: Vec<LabelId>,
         props: Vec<(PropertyKeyId, Value)>,
     ) -> Result<NodeId, GraphStoreError> {
+        todo!()
+    }
+
+    pub fn node_delete(&self, node_id: NodeId) -> Result<bool, GraphStoreError> {
+        todo!()
+    }
+
+    /// return number of deleted relationships
+    pub fn node_detach_delete(&self, node_id: NodeId) -> Result<u64, GraphStoreError> {
+        todo!()
+    }
+
+    /// return true if label added
+    pub fn node_add_label(&self, node_id: NodeId, label: LabelId) -> Result<bool, GraphStoreError> {
+        todo!()
+    }
+
+    pub fn node_remove_label(&self, node_id: NodeId, label: LabelId) -> Result<bool, GraphStoreError> {
+        todo!()
+    }
+
+    pub fn node_set_property(&self, key: PropertyKeyId, value: PropertyValue) -> Result<(), GraphStoreError> {
+        todo!()
+    }
+
+    pub fn node_remove_property(&self, key: PropertyKeyId) -> Result<(), GraphStoreError> {
+        todo!()
+    }
+
+    pub fn node_apply_changes(
+        &self,
+        added_label: Vec<LabelId>,
+        removed_label: Vec<LabelId>,
+        properties: Vec<(PropertyKeyId, PropertyValue)>,
+    ) -> Result<(), GraphStoreError> {
         todo!()
     }
 }
--- src/storage2/src/transaction/relationship.rs
+++ src/storage2/src/transaction/relationship.rs
@@ -1,4 +1,6 @@
-use mojito_common::{NodeId, PropertyKeyId, RelationshipId, RelationshipTypeId, value::Value};
+use mojito_common::{
+    NodeId, PropertyKey, PropertyKeyId, RelationshipId, RelationshipTypeId, store_types::PropertyValue, value::Value,
+};
 
 use crate::{error::GraphStoreError, transaction::Transaction};
 
@@ -10,6 +12,27 @@ impl Transaction {
         end_node_id: NodeId,
         props: Vec<(PropertyKeyId, Value)>,
     ) -> Result<RelationshipId, GraphStoreError> {
+        todo!()
+    }
+
+    /// return true on relationship deleted
+    pub fn relationship_delete(&self, rel_id: RelationshipId) -> Result<bool, GraphStoreError> {
+        todo!()
+    }
+
+    pub fn relationship_set_property(&self, key: PropertyKeyId, value: PropertyValue) -> Result<(), GraphStoreError> {
+        todo!()
+    }
+
+    pub fn relationship_remove_property(&self, key: PropertyKeyId) -> Result<(), GraphStoreError> {
+        todo!()
+    }
+
+    pub fn relationship_apply_changes(
+        &self,
+        rel_id: RelationshipId,
+        props: Vec<(PropertyKeyId, PropertyValue)>,
+    ) -> Result<(), GraphStoreError> {
         todo!()
     }
 }
