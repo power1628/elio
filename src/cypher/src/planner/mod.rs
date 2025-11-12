f3a99565f6ac4b757f16f2bd17c1133d331eb3ba
power1628 <pgplus1628@qq.com>
2025-11-12 14:58:18 +0800

    feat(planner): implement root query planning and single query head planning

--- src/cypher/src/planner/mod.rs
+++ src/cypher/src/planner/mod.rs
@@ -1,5 +1,10 @@
 use std::sync::Arc;
 
+use indexmap::IndexMap;
+use mojito_common::variable::VariableName;
+
+use crate::ir::query::{IrQuery, IrQueryRoot};
+use crate::planner::single_query::plan_single_query;
 use crate::{plan_context::PlanContext, plan_node::PlanExpr};
 
 use crate::{error::PlanError, ir::query::IrSingleQueryPart};
@@ -17,4 +22,26 @@ pub struct PlannerContext {
 
 pub struct PlannerConfig {
     // planner options here
+}
+
+pub struct RootPlan {
+    pub plan: Box<PlanExpr>,
+    pub names: IndexMap<VariableName, String>,
+}
+
+pub fn plan_root(
+    ctx: &mut PlannerContext,
+    _root @ IrQueryRoot { inner, names }: &IrQueryRoot,
+) -> Result<RootPlan, PlanError> {
+    let IrQuery { queries, union_all: _ } = inner;
+    assert!(!queries.is_empty());
+    if queries.len() > 1 {
+        return Err(PlanError::not_supported("Union all is not supported yet".to_string()));
+    }
+
+    let plan = plan_single_query(ctx, &queries[0])?;
+    Ok(RootPlan {
+        plan,
+        names: names.clone(),
+    })
 }
