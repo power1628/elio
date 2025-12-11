dd37028173da688ddf7bce13d1f9779871844e19
power1628 <pgplus1628@qq.com>
2025-12-11 15:08:43 +0800

    feat(test): make logictest runtime lazylock

--- src/logictest/tests/drive.rs
+++ src/logictest/tests/drive.rs
@@ -1,11 +1,19 @@
 extern crate logictest;
 
 use std::path::Path;
+use std::sync::LazyLock;
 
 use logictest::graphdb::{EmbeddedGraphDB, graphdb_column_validator};
 use sqllogictest::runner::Runner;
 use sqllogictest::{default_normalizer, default_validator};
 use tempfile::tempdir;
+
+static RT: LazyLock<tokio::runtime::Runtime> = LazyLock::new(|| {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+});
 
 datatest_stable::harness! {{test=run_slt_file, root="tests/basic/", pattern=r".*"},}
 
@@ -16,11 +24,6 @@ fn run_slt_file(path: &Path) -> datatest_stable::Result<()> {
         Ok(db)
     };
 
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .expect("failed to build tokio runtime");
-
     let mut runner = Runner::new(make_conn);
 
     // get env, if REWRITE=1, call run_file_async, else call run_file
@@ -30,9 +33,9 @@ fn run_slt_file(path: &Path) -> datatest_stable::Result<()> {
         let validator = default_validator;
         let normalizer = default_normalizer;
         let column_type_validator = graphdb_column_validator;
-        rt.block_on(runner.update_test_file(path, col_separator, validator, normalizer, column_type_validator))?;
+        RT.block_on(runner.update_test_file(path, col_separator, validator, normalizer, column_type_validator))?;
     } else {
-        rt.block_on(runner.run_file_async(path))?;
+        RT.block_on(runner.run_file_async(path))?;
     }
 
     Ok(())
